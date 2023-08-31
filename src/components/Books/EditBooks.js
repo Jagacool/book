aa7fb27607116b776931e62465791a06f371e684
j <137029997+Jagacool@users.noreply.github.com>
2023-08-31 20:18:49 +0530

    Update EditBooks.js

--- src/components/Books/EditBooks.js
+++ src/components/Books/EditBooks.js
@@ -1,86 +1,119 @@
 import { useFormik } from 'formik';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import * as yup from 'yup';
+import * as yup from "yup";
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
-
+import axios from "axios";
 const formvalidationSchema = yup.object({
-  book_name: yup.string().required().min(4),
-  book_count: yup.number().required(),
-  author: yup.string().required().min(4),
-  description: yup.string().required().min(5),
-});
+
+    book_name: yup
+        .string()
+        .required().min(4),
+    book_count: yup
+        .number()
+        .required(),
+    author: yup
+        .string()
+        .required().min(4),
+    description: yup
+        .string()
+        .required().min(5)
+})
 
 function EditBooks() {
-  const [isLoading, setLoading] = useState(false);
-  const params = useParams();
+    const [isLoading, setLoading] = useState(false);
+    const params = useParams();
 
-  const getUsers = async () => {
-    try {
-      setLoading(true);
-      const details = await axios.get(`https://638dfe2b4190defdb753283c.mockapi.io/books/${params.id}`);
-      myFormik.setValues(details.data);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    let getUsers = async () => {
+        try {
+            const details = await axios.get(`https://638dfe2b4190defdb753283c.mockapi.io/books/${params.id}`);
+            myFormik.setValues(details.data);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
     }
-  };
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    getUsers();
-  }, [myFormik]); // Include getUsers in the dependency array
+    const myFormik = useFormik({
+        initialValues: {
 
-  const navigate = useNavigate();
+            book_name: "",
+            book_count: "",
+            author: "",
+            description: ""
+        },
+        validationSchema: formvalidationSchema,
+        onSubmit: async (values) => {
+            try {
+                setLoading(true);
+                await axios.put(`https://638dfe2b4190defdb753283c.mockapi.io/books/${params.id}`, values);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+            }
+            navigate("/portal/book");
 
-  const myFormik = useFormik({
-    initialValues: {
-      book_name: '',
-      book_count: '',
-      author: '',
-      description: '',
-    },
-    validationSchema: formvalidationSchema,
-    onSubmit: async (values) => {
-      try {
-        setLoading(true);
-        await axios.put(`https://638dfe2b4190defdb753283c.mockapi.io/books/${params.id}`, values);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-      navigate('/portal/book');
-    },
-  });
+        },
+    })
 
-  return (
-    <form className="container" onSubmit={myFormik.handleSubmit}>
-      <div className="row mt-4 ps-5">
-        <div className="col-lg-5  mt-5 m-auto">
-          <h3>Edit Book Details:</h3>
-          <div className="form-group">
-            <label htmlFor="book_name">Book Name:</label>
-            <input
-              type="text"
-              id="book_name"
-              className={`form-control ${myFormik.touched.book_name && myFormik.errors.book_name ? 'is-invalid' : 'is-valid'}`}
-              value={myFormik.values.book_name}
-              name="book_name"
-              placeholder="Enter Book Name"
-              onBlur={myFormik.handleBlur}
-              onChange={myFormik.handleChange}
-            />
-            {myFormik.touched.book_name && myFormik.errors.book_name && <div className="invalid-feedback">{myFormik.errors.book_name}</div>}
-          </div>
-          {/* ...other input fields */}
-          <button disabled={isLoading} type="submit" className="btn btn-primary create-btn">
-            {isLoading ? 'Loading...' : 'Update'}
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+    return (
+        <>
+            <form className="container" onSubmit={myFormik.handleSubmit}>
+                <div className="row mt-4 ps-5">
+                    <div className="col-lg-5  mt-5 m-auto">
+                        <input
+                            type="text"
+                            className={`form-control ${myFormik.touched.book_name && myFormik.errors.book_name ? "is-invalid" : "is-valid"}`}
+                            value={myFormik.values.book_name}
+                            name="book_name"
+                            placeholder="Name"
+                            onBlur={myFormik.handleBlur}
+                            onChange={myFormik.handleChange}
+                        /><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.book_name && myFormik.errors.book_name ? myFormik.errors.book_name : null}</span><br />
+                        <input
+                            type="text"
+                            className={`form-control ${myFormik.touched.book_count && myFormik.errors.book_count ? "is-invalid" : "is-valid"}`}
+                            value={myFormik.values.book_count}
+                            name="book_count"
+                            placeholder="Enter book count"
+                            onBlur={myFormik.handleBlur}
+                            onChange={myFormik.handleChange}
+                        /><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.book_count && myFormik.errors.book_count ? myFormik.errors.book_count : null}</span><br />
+                        <input
+                            type="text"
+                            className={`form-control ${myFormik.touched.author && myFormik.errors.author ? "is-invalid" : "is-valid"}`}
+                            value={myFormik.values.author}
+                            name="author"
+                            placeholder="Enter Author Name"
+                            onBlur={myFormik.handleBlur}
+                            onChange={myFormik.handleChange}
+                        /><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.author && myFormik.errors.author ? myFormik.errors.author : null}</span><br />
+                        <textarea rows="3" cols="3"
+                            type="text"
+                            className={`form-control ${myFormik.touched.description && myFormik.errors.description ? "is-invalid" : "is-valid"}`}
+                            value={myFormik.values.description}
+                            name="description"
+                            placeholder="Enter book's description"
+                            onBlur={myFormik.handleBlur}
+                            onChange={myFormik.handleChange}>
+                        </textarea><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.description && myFormik.errors.description ? myFormik.errors.description : null}</span><br />
+                        <div className="d-sm-flex  justify-content-end mt-3">
+                            <button disabled={isLoading} type="submit" className="btn btn-primary create-btn">
+                                {isLoading ? "isLoading" : "Update"}
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </form>
+        </>
+    )
 }
 
-export default EditBooks;
+export default EditBooks
