7eb04857343d76f6bcdfeb9ddec2bb3c27333636
j <137029997+Jagacool@users.noreply.github.com>
2023-08-31 08:08:10 +0530

    Update AddBook.js

--- src/components/Books/AddBook.js
+++ src/components/Books/AddBook.js
@@ -2,10 +2,9 @@ import { useFormik } from 'formik';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import * as yup from "yup";
-import { useParams } from 'react-router-dom';
 import axios from "axios";
-const formvalidationSchema = yup.object({
 
+const formvalidationSchema = yup.object({
     book_name: yup
         .string()
         .required().min(4),
@@ -22,13 +21,11 @@ const formvalidationSchema = yup.object({
 
 function AddBook() {
     const [isLoading, setLoading] = useState(false);
-    const params = useParams();
 
     const navigate = useNavigate();
 
     const myFormik = useFormik({
         initialValues: {
-
             book_name: "",
             book_count: "",
             author: "",
@@ -44,7 +41,6 @@ function AddBook() {
                 setLoading(false);
             }
             navigate("/portal/book");
-
         },
     })
 
@@ -53,54 +49,18 @@ function AddBook() {
             <form className="container" onSubmit={myFormik.handleSubmit}>
                 <div className="row mt-4 ps-5">
                     <div className="col-lg-5  mt-5 m-auto">
-                        <input
-                            type="text"
-                            className={`form-control ${myFormik.touched.book_name && myFormik.errors.book_name ? "is-invalid" : "is-valid"}`}
-                            value={myFormik.values.book_name}
-                            name="book_name"
-                            placeholder="Enter Book Name"
-                            onBlur={myFormik.handleBlur}
-                            onChange={myFormik.handleChange}
-                        /><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.book_name && myFormik.errors.book_name ? myFormik.errors.book_name : null}</span><br />
-                        <input
-                            type="text"
-                            className={`form-control ${myFormik.touched.book_count && myFormik.errors.book_count ? "is-invalid" : "is-valid"}`}
-                            value={myFormik.values.book_count}
-                            name="book_count"
-                            placeholder="Enter book count"
-                            onBlur={myFormik.handleBlur}
-                            onChange={myFormik.handleChange}
-                        /><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.book_count && myFormik.errors.book_count ? myFormik.errors.book_count : null}</span><br />
-                        <input
-                            type="text"
-                            className={`form-control ${myFormik.touched.author && myFormik.errors.author ? "is-invalid" : "is-valid"}`}
-                            value={myFormik.values.author}
-                            name="author"
-                            placeholder="Enter Author Name"
-                            onBlur={myFormik.handleBlur}
-                            onChange={myFormik.handleChange}
-                        /><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.author && myFormik.errors.author ? myFormik.errors.author : null}</span><br />
-                        <textarea rows="3" cols="3"
-                            type="text"
-                            className={`form-control ${myFormik.touched.description && myFormik.errors.description ? "is-invalid" : "is-valid"}`}
-                            value={myFormik.values.description}
-                            name="description"
-                            placeholder="Enter book's description"
-                            onBlur={myFormik.handleBlur}
-                            onChange={myFormik.handleChange}>
-                        </textarea><span style={{ color: "red", fontSize: ".5" }} >{myFormik.touched.description && myFormik.errors.description ? myFormik.errors.description : null}</span><br />
+                        {/* Your input fields and form controls */}
+                        {/* ... */}
                         <div className="d-sm-flex  justify-content-end mt-3">
                             <button disabled={isLoading} type="submit" className="btn btn-primary create-btn">
                                 {isLoading ? "isLoading" : "Add"}
                             </button>
                         </div>
                     </div>
-
                 </div>
-
             </form>
         </>
     )
 }
 
-export default AddBook
+export default AddBook;
